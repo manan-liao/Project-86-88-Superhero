49967b4118b181339a2f4a0cf5795b591b2738d8
manan-liao <77179093+manan-liao@users.noreply.github.com>
2021-04-15 14:55:55 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+var canvas = new fabric.Canvas("myCanvas");
+
+player_x = 10;
+player_y = 10;
+block_width = 30;
+block_height = 30;
+player_object = "";
+block_object = "";
+
+function player_update(){
+    fabric.Image.fromURL("player.png", function (Img){
+        player_object = Img;
+        player_object.scaleToWidth(150);
+        player_object.scaleToHeight(140);
+        player_object.set({
+            top:player_y,
+            left:player_x
+        });
+        canvas.add(player_object);
+    });
+}
+
+function block_update(block_Img){
+    fabric.Image.fromURL(block_Img, function (Img){
+        block_object = Img;
+        block_object.scaleToWidth(block_width);
+        block_object.scaleToHeight(block_height);
+        block_object.set({
+            top:player_y,
+            left:player_x
+        });
+        canvas.add(block_object);
+    });
+}
+
