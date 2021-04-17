ad326d855c4d4c0745324601e6c870f170db0429
manan-liao <77179093+manan-liao@users.noreply.github.com>
2021-04-17 11:44:43 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -33,3 +33,58 @@ function block_update(block_Img){
     });
 }
 
+window.addEventListener("keydown",my_keydown);
+function my_keydown(e){
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+
+    if(e.shiftKey == true && keyPressed == "80"){
+        console.log("p and shift pressed together");
+        block_width = block_width + 10;
+        block_height = block_height + 10;
+        document.getElementById("blockWidth").innerHTML = block_width;
+        document.getElementById("blockHeight").innerHTML = block_height;
+    }
+    
+    if(e.shiftKey == true && keyPressed == "77"){
+        console.log("m and shiftkey pressed together");
+        block_width = block_width - 10;
+        block_height = block_height - 10;
+        document.getElementById("blockWidth").innerHTML = block_width;
+        document.getElementById("blockHeight").innerHTML = block_height;
+    }
+
+    if(keyPressed == "38"){
+        console.log("up");
+    }
+    if(keyPressed == "40"){
+        console.log("down");
+    }
+    if(keyPressed == "37"){
+        console.log("left");
+    }
+    if(keyPressed == "39"){
+        console.log("right");
+    }
+
+    if(keyPressed == "70"){
+        console.log("f");
+        block_update("hulk_face.png");
+    }
+    if(keyPressed == "66"){
+        console.log("b");
+        block_update("hulkd_body.png")
+    }
+    if(keyPressed == "76"){
+        console.log("l");
+        block_update("hulk_legs.png");
+    }
+    if(keyPressed == "72"){
+        console.log("h");
+        block_update("hulk_left_hand.png");
+    }
+    if(keyPressed == "82"){
+        console.log("r");
+        block_update("hulk_right_hand.png");
+    }
+}
